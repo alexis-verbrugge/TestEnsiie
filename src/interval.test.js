7be8f42f3e0c3184123de13b062713146b9add08
Alexis Verbrugge <alexisverbrugge77190@hotmail.fr>
2019-09-26 16:40:19 +0200

    overlaps 2

--- src/interval.test.js
+++ src/interval.test.js
@@ -3,21 +3,26 @@ const Interval = require('./interval');
 
 describe('Overlaps', function () {
 
-	var interval = new Interval(0,0);
+	var interval = new Interval(0,5);
 
 	test('Overlaps : interval(1,5) => true', () => {
 		var intervalTrue = new Interval(1, 5);
     	expect(intervalTrue.overlaps(intervalTrue)).toBe(true);
 	});
 
-	test('Overlaps : interval(5,1) => false', () => {
-		var intervalFalse = new Interval(5, 1);
+	test('Overlaps : interval(6,10) => false', () => {
+		var intervalFalse = new Interval(6, 10);
     	expect(intervalFalse.overlaps(intervalFalse)).toBe(false);
 	});
 
-	test('Overlaps : interval(1,1) => false', () => {
+	test('Overlaps : interval(1,1) => true', () => {
 		var intervalEgal = new Interval(1, 1);
-    	expect(intervalEgal.overlaps(intervalEgal)).toBe(false);
+    	expect(intervalEgal.overlaps(intervalEgal)).toBe(true);
 	});
+
+});
+
+
+describe('Includes', function () {
 
 });
