3fc6e072949b4138ff333486d5b8e990ac42d6a0
Alexis Verbrugge <alexisverbrugge77190@hotmail.fr>
2019-09-26 17:39:13 +0200

    union

--- src/interval.js
+++ src/interval.js
@@ -43,7 +43,7 @@ class Interval {
      * @returns {boolean}
      */
     includes(interval) {
-
+        return this.start <= interval.start && this.end >= interval.end;
     };
 
     /**
@@ -63,7 +63,31 @@ class Interval {
      * @returns {Interval[]}
      */
     union(interval) {
+        var intervals = [];
 
+        if (this.overlaps(interval) == false) {
+            intervals[0]=this;
+            intervals[1]=interval;
+        } else {
+        
+            var startUnion = 0;
+            var endUnion = 0;
+
+            if (interval.start >= this.start) {
+                startUnion = this.start
+            } else {
+                startUnion = interval.start
+            }
+
+            if (interval.end <= this.end) {
+                endUnion = this.end
+            } else {
+                endUnion = interval.end
+            }
+            var newIterval = new Interval(startUnion, endUnion);
+            intervals.push(newIterval);
+        }
+        return intervals;
     };
 
     /**
