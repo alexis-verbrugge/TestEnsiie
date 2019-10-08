eea267414b1dcf0deb268d93ca760985a497d056
Alexis Verbrugge <alexisverbrugge77190@hotmail.fr>
2019-10-08 16:45:22 +0200

    get book by name

--- src/book.repository.test.js
+++ src/book.repository.test.js
@@ -2,8 +2,6 @@ const BookRepository = require('./book.repository');
 
 
 
-
-
 describe('Book repository Save', function () {
 
 const dbMock = {
@@ -20,6 +18,7 @@ const dbMock = {
 });
 
 
+//###########
 describe('Book repository Total count', function () {
 
     const dbMock = {
@@ -36,17 +35,38 @@ describe('Book repository Total count', function () {
 
 });
 
+//###########
 describe('Book repository Total price', function () {
     const dbMock = {
             get : jest.fn().mockReturnThis(),
             map : jest.fn().mockReturnThis(),
-            value : jest.fn().mockReturnValue(30)
+            size : jest.fn().mockReturnThis(),
+            value : jest.fn().mockReturnValueOnce([15.0, 5.0, 10.0])
         };
 
     test('Count Total Price => 30', () => {
 
         const repository = new BookRepository(dbMock);
-        expect(repository.getTotalPrice()).toBe(30);
+        expect(repository.getTotalPrice()).toBe(30.0);
+    });
+
+});
+
+//###########
+describe('Book repository find by name', function () {
+
+    var one_piece = {"id": 2, "name": "One piece", "price": 7.5, "added_at": "2019-01-10"};
+    
+    const dbMock = {
+            get : jest.fn().mockReturnThis(),
+            find : jest.fn().mockReturnThis(),
+            value : jest.fn().mockReturnValue(one_piece)
+        };
+
+    test('get book by Name ', () => {
+
+        const repository = new BookRepository(dbMock);
+        expect(repository.getBookByName("One piece")).toEqual(one_piece);
     });
 
 });
